736205802cac860839ac6c7136dbfc20d24b448b
charlo-funn <funnekoc@mcmaster.ca>
2024-03-04 14:37:10 -0500

    lab3 refactor cont, added up and downsample

--- include/filter.h
+++ include/filter.h
@@ -16,7 +16,9 @@ Ontario, Canada
 // declaration of a function prototypes
 void impulseResponseLPF(float, float, unsigned short int, std::vector<float> &);
 void convolveFIR(std::vector<float> &, const std::vector<float> &, const std::vector<float> &);
-void blockConvolveFIR(std::vector<float> &y, const std::vector<float> &x, const std::vector<float> &h, std::vector<float> &state, int &position, int block_size);
+void blockConvolveFIR(std::vector<float> &y, const std::vector<float> x, const std::vector<float> h, std::vector<float> &state, int position, int block_size);
 void fmDemodArctan(std::vector<float> I, std::vector<float> Q, float &prev_I, float &prev_Q, std::vector<float>& fm_demod);
+void downsample(const std::vector<float> data, size_t factor, std::vector<float>& downsampled);
+void upsample(const std::vector<float> data, size_t factor, std::vector<float> &upsampled);
 
 #endif // DY4_FILTER_H
