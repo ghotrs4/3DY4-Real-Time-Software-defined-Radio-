dc5563a258707ac560d3b5e1110a4cd7e6651b4b
Isaac <thomai4@mcmaster.ca>
2024-03-27 23:52:10 -0400

    fixed fmdemod

--- src/filter.cpp
+++ src/filter.cpp
@@ -102,9 +102,8 @@ void fmDemodArctan(const std::vector<float> &I, const std::vector<float> &Q, flo
 }
 
 void fmDemodArctanOptimized(const std::vector<float> &I, const std::vector<float> &Q, float &prev_phase, std::vector<float>& fm_demod){
-	fm_demod.resize(I.size());
 	float current_phase;
-	float delta_phase;
+	fm_demod.resize(I.size());
 	for(int k=0;k<I.size();k++){
 		if(I[k]==0 && Q[k]==0){
 			current_phase=0;
@@ -112,17 +111,16 @@ void fmDemodArctanOptimized(const std::vector<float> &I, const std::vector<float
 		else{
 			current_phase=std::atan2(Q[k], I[k]);
 		}
-		if(k>0){ //begin phase unwrap (python conversion)
-			delta_phase = current_phase - prev_phase;
-            if(delta_phase > PI) {
-                current_phase -= 2 * PI;
-            } else if (delta_phase < -PI) {
-                current_phase += 2 * PI;
-            }
+		while(current_phase - prev_phase > PI){
+			current_phase -=2*PI;
 		}
-        fm_demod[k] = current_phase - prev_phase;
-        prev_phase = current_phase;
+		while(current_phase - prev_phase < -PI){
+			current_phase +=2*PI;
+		}
+		fm_demod[k] = current_phase - prev_phase;
+		prev_phase = current_phase;
 	}
+	
 }
 
 void downsample(const std::vector<float> data, size_t factor, std::vector<float>& downsampled) {
